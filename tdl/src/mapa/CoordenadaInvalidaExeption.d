ca95540d108f2ec207f889f10790beab303ca5fd
RamiroChogri <32467892+RamiroChogri@users.noreply.github.com>
2019-11-12 19:26:35 -0300

    Update CoordenadaInvalidaExeption.d

--- tdl/src/mapa/CoordenadaInvalidaExeption.d
+++ tdl/src/mapa/CoordenadaInvalidaExeption.d
@@ -1,7 +1,7 @@
-module mapa.CoordenadaInvalidaExepcion;
+module mapa.CoordenadaInvalidaExeption;
 import std.exception;
 
-public class CoordenadaInvalidaExepcion : Exception{
+public class CoordenadaInvalidaExeption : Exception{
 	this(string msg, string file = __FILE__, size_t line = __LINE__) {
         super(msg, file, line);
     }
